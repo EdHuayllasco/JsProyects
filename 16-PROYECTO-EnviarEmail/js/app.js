ca0c3c4bb0453f5f4a8469bd93fd6875638a2fde
Edward Huayllasco <edwardhc1997@gmail.com>
2023-01-23 18:32:44 -0500

    proyecto finalizado

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -1,28 +1,76 @@
 document.addEventListener('DOMContentLoaded', function(){
     //SELECCIONAR ELEMENTOS DE LA INTERFAZ
+    const emailcompleto = {
+        email : '',
+        asunto: '',
+        mensaje: ''
+    };
     const email = document.querySelector('#email');
     const asunto = document.querySelector('#asunto');
     const mensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]');
+    const spinner = document.querySelector("#spinner");
     //ASIGNAR EVENTOS BLUR SALIENDO DEL CAMPO, INPUT TIEMPO REAL
-    email.addEventListener('blur', ValidarCampo);
-    asunto.addEventListener('blur', ValidarCampo); 
-    mensaje.addEventListener('blur', ValidarCampo);
+    email.addEventListener('input', ValidarCampo);
+    asunto.addEventListener('input', ValidarCampo); 
+    mensaje.addEventListener('input', ValidarCampo);
+    formulario.addEventListener('submit', enviarEmail);
+    btnReset.addEventListener('click', e => {
+        e.preventDefault();
+        resetFormulario();
+
+    });
+    function enviarEmail(e){
+        e.preventDefault();
+        spinner.classList.add("flex");
+        spinner.classList.remove("hidden");
+        setTimeout(()=>{
+            spinner.classList.remove("flex");
+            spinner.classList.add("hidden");
+            resetFormulario();
+            //CREAR UNA ALERTA EXITO
+            const alertaExito = document.createElement('P');
+            alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center' , 'rounded-lg' , 'mt-10', 'font-bold', 'text-sm', 'uppercase');
+            alertaExito.textContent = "Envio existoso";
+            formulario.appendChild(alertaExito);
+            
+        }, 3000);
+        
+        setTimeout(()=>{
+            const alertaexito = document.querySelector(".bg-green-500");
+            console.log(alertaexito);
+            alertaexito.remove();
+        },4000);
+        
+    }
     function ValidarCampo(e){
         //nextElementSibling = recorrer el dom al hermano.
         const padre = e.target.parentElement;
         // trim = desaparece los espacios
         if ( e.target.value.trim() === ""){
+            emailcompleto[e.target.id] = "";
+            VerificarEmaillleno();
             const nombre = e.target.id;            
-            mostraralerta(nombre, padre);
+            mostraralerta(`El espacio ${nombre} esta vacio`, padre);
             return;  
         }
+        if(e.target.id === "email" && !ComprobarEmail(e.target.value)){
+            emailcompleto[e.target.id] = "";
+            VerificarEmaillleno();
+            mostraralerta('El email es incorrecto', padre);
+            return;
+        }
         limpiarAlerta(padre);
+        //ASIGNAR VALORES
+        emailcompleto[e.target.id] = e.target.value.trim().toLowerCase();
+        VerificarEmaillleno();
     }
-    function mostraralerta(nombre,padre){
+    function mostraralerta(mensaje,padre){
         limpiarAlerta(padre);
         const error = document.createElement('P');
-        error.textContent = `El espacio ${nombre} esta vacio`;
+        error.textContent = mensaje;
         error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
         padre.appendChild(error);
     }
@@ -33,5 +81,29 @@ document.addEventListener('DOMContentLoaded', function(){
             // eliminamos el elemento que contiene bg-red-600
             alerta.remove();
         }
+    }
+    function ComprobarEmail(email){
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ ;
+        const resultado = regex.test(email);
+        return resultado;
+    }
+    function VerificarEmaillleno(){
+        //CREA UN ARREGLO CON LOS VALORES DEL OBJETO
+        // INCLUDE '' VERIFICA SI ALGUN ESPACIO ESTA VACIO ENVIA TRUE
+        // CUANDO APAREZCA FALSE SIGNIFICA QUE TODOS LOS ESPACIO ESTAN LLENADOS COMPLETAMENTE Y CORRECTAMENTE
+        if(Object.values(emailcompleto).includes('')){
+            btnSubmit.classList.add('opacity-50');
+            btnSubmit.disabled = true;
+            return;
+        }
+        btnSubmit.classList.remove('opacity-50');
+        btnSubmit.disabled = false;
+    }
+    function resetFormulario (){
+        emailcompleto.email = '';
+        emailcompleto.asunto = '';
+        emailcompleto.mensaje = '';
+        formulario.reset();
+        VerificarEmaillleno();
     }
 }); 
