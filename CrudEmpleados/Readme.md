de01305f1d289bc5eb0358d4da299337fffcb24e
Edward Huayllasco <edwardhc1997@gmail.com>
2023-02-28 23:36:06 -0500

    Readme actualizado

---
+++ CrudEmpleados/Readme.md
@@ -0,0 +1,5 @@
+## Despliegue del proyecto base en: 
+
+[CRUD-empleados](https://crud-empleadosed.netlify.app/)
+
+## Despliegue del proyecto mejorado en: 
--- CrudEmpleados/js/nuevocliente.js
+++ CrudEmpleados/js/nuevocliente.js
@@ -6,16 +6,7 @@
         conectarDB();
         formulario.addEventListener('submit', validarCliente);
     })
-    function conectarDB(){
-        const abrirConexion = window.indexedDB.open('crm',1);
-        abrirConexion.onerror = ()=>{
-            console.log('hubo un error');
-        }
-        abrirConexion.onsuccess = () =>{
-            console.log('Conexion exitosa');
-            DB = abrirConexion.result;
-        }
-    }
+    
     function validarCliente(e){
         e.preventDefault();
         //leer todos los inputs
@@ -38,24 +29,7 @@
         cliente.id = Date.now();
         crearNuevoCliente(cliente);
     }
-    function imprimirAlerta (mensaje, alerta){
-        const div = document.createElement('div');
-        const alertaDiv = document.querySelector('.alerta');
-        if(!alertaDiv){
-            div.classList.add('px-4', 'py-3', 'rounded', 'max-w-lg', 'mt-6', 'text-center','border','alerta');
-            if(alerta === 'error'){
-                div.classList.add('bg-red-100', 'border-red-400', 'text-red-700');
-            }else {
-                div.classList.add('bg-green-100', 'border-green-400', 'text-green-700');
-            }
-            div.textContent = mensaje;
-            formulario.appendChild(div);
-            setTimeout(() => {
-                div.remove();
-            }, 3000);
-        }
-        
-    }
+    
     function crearNuevoCliente(cliente) {
         const transaction = DB.transaction(['crm'],'readwrite');
         const objectStore = transaction.objectStore('crm');
